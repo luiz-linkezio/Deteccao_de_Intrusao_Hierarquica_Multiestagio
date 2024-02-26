b2c490391070f87a2ff8c02383fb442a310bdadc
Raafm <raafm@cin.ufpe.br>
2024-02-26 12:31:58 -0300

    add classe integrando os estagios

--- SistemaHierarquico.py
+++ SistemaHierarquico.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import classification_report
 from sklearn.datasets import load_digits
 from FirstStage import FirstStage
@@ -10,12 +11,12 @@ class Sistema_Hierarquico_Base(FirstStage, SecondStage, Extension):
     def __init__(self, anomaly_detector, classifier, n_anomalias, threshold_b, threshold_m, threshold_u):
         assert threshold_b <  threshold_u # not sure why
 
-        FirstStage().__init__(anomaly_detector, threshold_b, benign_label= n_anomalias + 1)
-        SecondStage().__init__(classifier, n_anomalias, threshold_m)
-        Extension().__init__(threshold_u)
+        FirstStage.__init__(self,anomaly_detector, threshold_b)
+        SecondStage.__init__(self,classifier, n_anomalias, threshold_m)
+        Extension.__init__(self,threshold_u)
 
         self.n_anomalias = n_anomalias
-
+        self._benign_label = n_anomalias+1
 
     def get_labels(self):
         print("0 means zero-day, the system do not recognize as benign or any known malign")
@@ -31,14 +32,13 @@ class Sistema_Hierarquico_Base(FirstStage, SecondStage, Extension):
 
         # segundo estagio: classificador de ataque
         # atk e um vetor com os labels (de 1 ate n_anomalias) e com alguns unkown (-1)
-        atk = self.predict_atack(X[possibly_malign_indices])
+        atk = self.predict_attack(X[possibly_malign_indices])
 
         possibly_zero_day_indices = possibly_malign_indices[atk == self._unknown_label]
 
         # inferimos o zero day
         # nao precisamos definir os outros elementos como benignos, pois isso foi feito por default no inicio desta funcao
         is_not_zero_day = self.predict_benign_zero_day(prob_anomalia[possibly_zero_day_indices])
-        zero_day_indices = possibly_zero_day_indices[is_not_zero_day == False]
 
         # n_classes+1 representa zero-day,
         # [1,2,..., n_anomalias] representam anomalias
@@ -64,3 +64,51 @@ if __name__ == '__main__':
             # cada coluna representa uma classe
             # cada elemento da matriz representa uma probabilidade
             return self.classifier.predict_proba(X)
+
+if __name__ == '__main__':
+    """Drive Test Code"""
+    def criar_dados(X,y):
+
+        # imagens 6,7,8,9 sao benignos
+        X_benign = X[y > 5]
+        y_benign = y[y > 5]
+
+        # imagens 1,2,3,4,5 sao anomalias
+        X_anomalia = X[(y <= 5) & (y != 0)]
+        y_anomalia = y[(y <= 5) & (y != 0)]
+
+        # imagem 0 e zero-day
+        X_zero_day = X[y == 0]
+        y_zero_day = y[y == 0]
+
+        return X_benign, y_benign, X_anomalia, y_anomalia, X_zero_day, y_zero_day
+
+    def treinar_modelos(X_anomalia,y_anomalia,X_benign):
+            
+        detector_anomalia = DecisionTreeClassifier(min_samples_split= 10)
+        classificador = MLPClassifier((10,10,10))
+        X_tree = np.row_stack((X_anomalia,X_benign))
+        # 0 ---> anomalia
+        # 1 ---> benign
+        y_tree = np.concatenate(
+            (np.zeros(len(X_anomalia)), np.ones(len(X_benign)))
+        )
+
+        detector_anomalia.fit(X_tree,y_tree)
+            
+        classificador.fit(X_anomalia, y_anomalia)
+        return detector_anomalia, classificador
+
+    X,y = load_digits(return_X_y= True)
+    X_benign, y_benign, X_anomalia, y_anomalia, X_zero_day, y_zero_day = criar_dados(X,y)
+    detector_anomalia, classificador = treinar_modelos(X_anomalia,y_anomalia,X_benign) 
+
+    n_anomalias = 5
+    sistema = SistemaHierarquico(detector_anomalia, classificador, n_anomalias,.5,.9,.6)
+    y = np.where(y > n_anomalias, sistema._benign_label, y)
+
+    y_pred = sistema.predict(X)
+    
+    print("Primeiro estagio")
+    print(classification_report(y,y_pred))
+    print("---"*50)
