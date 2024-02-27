dd366c54c66606c3a7006c7d5f9d70e18a08a249
Raafm <raafm@cin.ufpe.br>
2024-02-26 22:52:22 -0300

    add threshold search para FirstStage

--- SistemaHierarquico.py
+++ SistemaHierarquico.py
@@ -6,6 +6,7 @@ from sklearn.datasets import load_digits
 from FirstStage import FirstStage
 from SecondStage import SecondStage
 from Extension import Extension
+from sklearn.model_selection import train_test_split
 
 class Sistema_Hierarquico_Base(FirstStage, SecondStage, Extension):
     def __init__(self, anomaly_detector, classifier, n_anomalias, threshold_b, threshold_m, threshold_u):
@@ -28,13 +29,13 @@ class Sistema_Hierarquico_Base(FirstStage, SecondStage, Extension):
         prediction = np.zeros(len(X),dtype= int) + self._benign_label
 
         # primeiro estagio: detector de anomalias
-        possibly_malign_indices, prob_anomalia = self.detectar_anomalia(X)
+        possivelmente_anomalia_indices, prob_anomalia = self.detectar_anomalia(X)
 
         # segundo estagio: classificador de ataque
         # atk e um vetor com os labels (de 1 ate n_anomalias) e com alguns unkown (-1)
-        atk = self.predict_attack(X[possibly_malign_indices])
+        atk = self.predict_attack(X[possivelmente_anomalia_indices])
 
-        possibly_zero_day_indices = possibly_malign_indices[atk == self._unknown_label]
+        possibly_zero_day_indices = possivelmente_anomalia_indices[atk == self._unknown_label]
 
         # inferimos o zero day
         # nao precisamos definir os outros elementos como benignos, pois isso foi feito por default no inicio desta funcao
@@ -43,7 +44,7 @@ class Sistema_Hierarquico_Base(FirstStage, SecondStage, Extension):
         # n_classes+1 representa zero-day,
         # [1,2,..., n_anomalias] representam anomalias
         # 0 representa zero-day
-        prediction[possibly_malign_indices] = atk
+        prediction[possivelmente_anomalia_indices] = atk
         prediction[possibly_zero_day_indices] = np.where(is_not_zero_day, self._benign_label, self._zero_day_label)
 
         return prediction
@@ -57,7 +58,7 @@ if __name__ == '__main__':
             # vetor com classe de maior probabilidade de cada instancia
             # pegam so a probabilidade dos elementos serem 1
             probs = self.anomaly_detector.predict_proba(X)
-            return probs[:,0]
+            return probs[:,1]
 
         def classify_proba(self,X):
             # matriz, cada linha representa uma instancia que pde ser maligna,
@@ -86,12 +87,12 @@ if __name__ == '__main__':
     def treinar_modelos(X_anomalia,y_anomalia,X_benign):
             
         detector_anomalia = DecisionTreeClassifier(min_samples_split= 10)
-        classificador = MLPClassifier((10,10,10))
+        classificador = MLPClassifier((32,64,64,128,64,32))
         X_tree = np.row_stack((X_anomalia,X_benign))
-        # 0 ---> anomalia
-        # 1 ---> benign
+        # 1 ---> anomalia
+        # 0 ---> benign
         y_tree = np.concatenate(
-            (np.zeros(len(X_anomalia)), np.ones(len(X_benign)))
+            (np.ones(len(X_anomalia)), np.zeros(len(X_benign)))
         )
 
         detector_anomalia.fit(X_tree,y_tree)
@@ -100,15 +101,16 @@ if __name__ == '__main__':
         return detector_anomalia, classificador
 
     X,y = load_digits(return_X_y= True)
-    X_benign, y_benign, X_anomalia, y_anomalia, X_zero_day, y_zero_day = criar_dados(X,y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+    X_benign, y_benign, X_anomalia, y_anomalia, X_zero_day, y_zero_day = criar_dados(X_train,y_train)
     detector_anomalia, classificador = treinar_modelos(X_anomalia,y_anomalia,X_benign) 
 
     n_anomalias = 5
     sistema = SistemaHierarquico(detector_anomalia, classificador, n_anomalias,.5,.9,.6)
-    y = np.where(y > n_anomalias, sistema._benign_label, y)
+    y_test = np.where(y_test > n_anomalias, sistema._benign_label, y_test)
 
-    y_pred = sistema.predict(X)
+    y_pred = sistema.predict(X_test)
     
     print("Primeiro estagio")
-    print(classification_report(y,y_pred))
+    print(classification_report(y_test,y_pred))
     print("---"*50)
