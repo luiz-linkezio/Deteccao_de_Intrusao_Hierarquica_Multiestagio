19e5436af3dc1f1f63c3606c22c92c64fd086172
Raafm <raafm@cin.ufpe.br>
2024-03-12 08:54:27 -0300

    movido para o ipynb de tratamento

--- save_datasets.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-def save_datasets(list_datasets : list[pd.DataFrame], datasets_paths = list[str]) :
-    for dataset, dataset_path in zip(list_datasets, datasets_paths) :
-        dataset.to_csv(dataset_path)
-
